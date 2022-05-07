82478214d8c58b3071a1a73b2755424bb2c7d00b
TheJamHutch <james.hutch@protonmail.com>
2022-05-07 01:05:09 +0100

    Tiling tab layout

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -8,7 +8,7 @@ export const config = {
       lineDashSpeed: 4                    // Controls the speed of the line dash animation shown when selecting tiles.
     },
     palette: {
-      resolution: { x: 800, y: 300 },
+      resolution: { x: 600, y: 400 },
       cellSize: 32
     }
   };
